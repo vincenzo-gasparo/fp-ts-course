3668d0f6a6ee67ad4b203dda0db75453c123e79f
vincenzo-gasparo <vincenzo.gasparo@gmail.com>
2026-03-06 11:50:55 +0100

    refactor: move app files into apps/fp-ts-course

--- src/lib/mdx.ts
+++
@@ -1,7 +0,0 @@
-// Source: https://velite.js.org/guide/using-mdx
-import * as runtime from 'react/jsx-runtime'
-
-export function useMDXComponent(code: string) {
-  const fn = new Function(code)
-  return fn({ ...runtime }).default as React.ComponentType<{ components?: Record<string, React.ComponentType<any>> }>
-}
